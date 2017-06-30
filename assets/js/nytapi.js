d68dd7bdde6c81e2e4b257e887fea33ca5070698
a-bhuyan <arnob.bhuyan@gmail.com>
2017-06-30 14:32:34 -0500

    v3

--- assets/js/nytapi.js
+++ assets/js/nytapi.js
@@ -37,18 +37,27 @@ $(document).ready(function() {
 */
 		var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 		url += '?' + $.param({
-		  'api-key': "674576b4c7b548809ad5fb4c8909865b",
-		  'q': "obama",
-		  'begin_date': "20121228",
-		  'end_date': "20000210"
+		  'api-key': "7816a52515ce4ce0b8ef442c2a524c15",
+		  'q': searchTerm,
+		  'begin_date': startYear,
+		  'end_date': endYear
 		});
+
+			//url += "&fl=";
 
 		$.ajax({
 		  url: url,
 		  method: 'GET',
 		}).done(function(result) {
 	  	console.log(result);
+	  	$("#gifs-appear-here").html(result.response.docs[0].headline);
+
+	  	$("#title").append(result.response.docs[0].headline);
+	  	$("#author").append(result.response.docs[0].byline.original);
+	  	$("#section").append("Section: " + result.response.docs[0].section_name);
+
 		}).fail(function(err) {
+
 	  	throw err;
 		});
 
