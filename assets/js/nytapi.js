82fb5c11330f27d32abe3cb999fd5d003fcaab4d
a-bhuyan <arnob.bhuyan@gmail.com>
2017-06-30 11:58:12 -0500

    v2

--- assets/js/nytapi.js
+++ assets/js/nytapi.js
@@ -1,1 +1,25 @@
-var something = 10;
+$(document).ready(function() {
+
+	var searchTerm;
+	var numRecords;
+	var startYear;
+	var endYear;
+	
+	function clearSearch() {
+		searchTerm = "";
+		numRecords = "";
+		startYear = "";
+		endYear = "";
+	}
+
+	$("#searchButton").on("click", function() {
+
+		
+
+
+	});
+	$("#clearButton").on("click", function() {
+		clearSearch();
+	});
+	
+});
