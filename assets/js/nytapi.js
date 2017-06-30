ef1c9a4dbaed4284fb7e071011867da9beb14bf1
David DuChene <davidduchene@Davids-MacBook-Pro.local>
2017-06-30 12:54:02 -0500

    another try

---
+++ assets/js/nytapi.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+	var searchTerm;
+	var numRecords;
+	var startYear;
+	var endYear;
+	
+	function clearSearch() {
+		searchTerm = "";
+		numRecords = "";
+		startYear = "";
+		endYear = "";
+	}
+
+	$("#searchButton").on("click", function() {
+
+		
+
+
+	});
+	$("#clearButton").on("click", function() {
+		clearSearch();
+	});
+	
+});
